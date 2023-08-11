6914d9800c9f46fdedd696bbeaf68353beb3dfbb
Andrea Ramírez <77802937+Andrea-gt@users.noreply.github.com>
2023-08-11 17:41:05 -0600

    feature: Rendering of five polygons
    In this commit, I've updated the render function in order to render 5 polygons.

--- Framebuffer.cpp
+++ Framebuffer.cpp
@@ -116,6 +116,72 @@ void Framebuffer::render() {
     ColorRGB lineColor(255, 255, 255);
     setCurrentColor(lineColor);
 
+    // Polygon 1
+    std::vector<Vertex2> polygonPoints = {
+            {165, 380}, {185, 360},
+            {180, 330}, {207, 345},
+            {233, 330}, {230, 360},
+            {250, 380}, {220, 385},
+            {205, 410}, {193, 383}
+    };
+
+    drawPolygon(polygonPoints);
+
+    ColorRGB fillColor(189, 246, 252);
+    setCurrentColor(fillColor);
+
+    fillPolygon(polygonPoints);
+
+    // Polygon 2
+    std::vector<Vertex2> polygonPoints2 = {
+            {321, 335}, {288, 286},
+            {339, 251}, {374, 302}
+    };
+
+    drawPolygon(polygonPoints2);
+
+    ColorRGB fillColor2(249, 222, 169);
+    setCurrentColor(fillColor2);
+
+    fillPolygon(polygonPoints2);
+
+
+    // Polygon 3
+    std::vector<Vertex2> polygonPoints3 = {
+            {377, 249}, {411, 197}, {436, 249}
+    };
+
+    drawPolygon(polygonPoints3);
+
+    ColorRGB fillColor3(200, 152, 252);
+    setCurrentColor(fillColor3);
+
+    fillPolygon(polygonPoints3);
+
+
+    // Polygon 4
+    std::vector<Vertex2> polygonPoints4 = {
+            {413, 177}, {448, 159}, {502, 88}, {553, 53},
+            {535, 36}, {676, 37}, {660, 52}, {750, 145},
+            {761, 179}, {672, 192}, {659, 214}, {615, 214},
+            {632, 230}, {580, 230}, {597, 215}, {552, 214},
+            {517, 144}, {466, 180}
+    };
+
+    drawPolygon(polygonPoints4);
+
+    ColorRGB fillColor4(222, 244, 208);
+    setCurrentColor(fillColor4);
+    fillPolygon(polygonPoints4);
+
+    // Polygon 5
+    std::vector<Vertex2> polygonPoints1 = {
+            {682, 175}, {708, 120}, {735, 148}, {739, 170}
+    };
+
+    setCurrentColor(clearColor);
+    fillPolygon(polygonPoints1);
+
     writeBMP("out.bmp");
 }
 
